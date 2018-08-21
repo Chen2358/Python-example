8e826960ddfccb6bfe4ad05aa29c2c9061fe9ca1
Chen2358 <1430535115@qq.com>
2018-08-21 18:54:58 +0800

    Update app.py

--- flask-sse-chat/app.py
+++ flask-sse-chat/app.py
@@ -6,37 +6,42 @@ import redis
 
 app = flask.Flask(__name__)
 app.secret_key = 'asdf'
+#设置redis 链接
 red = redis.StrictRedis(host='localhost', port=6379, db=6)
 
-
+#消息生成器
 def event_stream():
     pubsub = red.pubsub()
+    #订阅'chat'频道
     pubsub.subscribe('chat')
-    # TODO: handle client disconnection.
+    # 开始监听消息，如果有消息则返回消息
     for message in pubsub.listen():
         print (message)
         yield 'data: %s\n\n' % message['data']
 
-
+#登录
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if flask.request.method == 'POST':
+        #记录用户信息
         flask.session['user'] = flask.request.form['user']
         return flask.redirect('/')
     return '<form action="" method="post">user: <input name="user">'
 
-
+#接受 Javascript post 的消息
 @app.route('/post', methods=['POST'])
 def post():
     message = flask.request.form['message']
     user = flask.session.get('user', 'anonymous')
     now = datetime.datetime.now().replace(microsecond=0).time()
+    #将消息发布到'chat'频道中
     red.publish('chat', u'[%s] %s: %s' % (now.isoformat(), user, message))
     return flask.Response(status=204)
 
-
+#事件流接口
 @app.route('/stream')
 def stream():
+    #返回的类型时'text/event-stream'
     return flask.Response(event_stream(),
                           mimetype="text/event-stream")
 
@@ -55,6 +60,7 @@ def home():
         <pre id="out"></pre>
         <script>
             function sse() {
+                //接入服务器的事件流
                 var source = new EventSource('/stream');
                 var out = document.getElementById('out');
                 source.onmessage = function(e) {
@@ -62,6 +68,7 @@ def home():
                     out.innerHTML =  e.data + '\\n' + out.innerHTML;
                 };
             }
+            // POST 消息到服务端
             $('#in').keyup(function(e){
                 if (e.keyCode == 13) {
                     $.post('/post', {'message': $(this).val()});
