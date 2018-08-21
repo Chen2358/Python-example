7d58964fed20f1edb60597bdd55e7f1491e9d5dd
chen <1430535115@qq.com>
2018-08-21 15:40:27 +0800

    实现剩下的部分

--- blog/app.py
+++ blog/app.py
@@ -35,18 +35,6 @@ database = flask_db.database
 oembed_providers = bootstrap_basic(OEmbedCache())
 
 
-@app.template_filter('clean_querystring')
-def clean_querystring(request_args, *keys_to_remove, **new_values):
-	querystring = dict((key, value) for key, value in request_args.items())
-	for key in keys_to_remove:
-		querystring.pop(key, None)
-	querystring.update(new_values)
-	return urllib.urlencode(querystring)
-
-@app.errorhandler(404)
-def not_found(exc):
-	return Response('<h3>Page not found</h3>'), 404
-
 
 class Entry(flask_db.Model):
 	title = CharField()
@@ -54,6 +42,18 @@ class Entry(flask_db.Model):
 	content = TextField()
 	published = BooleanField(index=True)
 	timestamp = DateTimeField(default=datetime.datetime.now, index=True)
+
+	@property
+	def html_content(self):
+		hilite = CodeHiliteExtension(linenums=False, css_class='highlight')
+		extras = ExtraExtension()
+		markdown_content = markdown(self.content, extensions=[hilite, extras])
+		oembed_content = parse_html(
+			markdown_content,
+			oembed_providers,
+			urlize_all=True,
+			maxwidth=app.config['SITE_WIDTH'])
+		return Markup(oembed_content)
 
 	def save(self, *args, **kwargs):
 		if not self.slug:
@@ -73,6 +73,33 @@ class Entry(flask_db.Model):
 			force_insert = False
 		fts_entry.content = '\n'.join((self.title, self.content))
 		fts_entry.save(force_insert=force_insert)
+
+	@classmethod
+	def public(cls):
+		return Entry.select().where(Entry.published == True)
+
+	@classmethod
+	def drafts(cls):
+		return Entry.select().where(Entry.published == False)
+
+	@classmethod
+	def search(cls, query):
+		words = [word.strip() for word in query.split() if word.strip()]
+		if not words:
+			return Entry.select().where(Entry.id == 0)
+		else:
+			search = ' '.join(words)
+		return (FTSEntry
+				.select(
+					FTSEntry,
+					Entry,
+					FTSEntry.rank().alias('score'))
+				.join(Entry, on(FTSEntry.entry.id == Entry.id).alias('entry'))
+				.where(
+					(Entry.published == True) &
+					(FTSEntry.match(search)))
+				.order_by(SQL('score').desc()))
+
 
 class FTSEntry(FTSModel):
 	entry_id = IntegerField()
@@ -108,6 +135,82 @@ def logout():
 		session.clear()
 		return redirect(url_for('login'))
 	return render_template('logout.html')
+
+@app.route('/')
+def index():
+	search_query = request.args.get('q')
+	if search_query:
+		query = Entry.search(search_query)
+	else:
+		query = Entry.public().order_by(Entry.timestamp.desc())
+	return object_list('index.html', query, search=search_query)
+
+@app.route('/create/', methods=['GET', 'POST'])
+@login_required
+def create():
+	if request.methods == 'POST':
+		if request.form.get('title') and request.form.get('content'):
+			entry = Entry.create(
+				title=request.form['title'],
+				content=request.form['title'],
+				published=request.form.get('published') or False)
+			flash('Entry created successfully.', 'sucess')
+			if entry.published:
+				return redirect(url_for('detail', slug=entry.slug))
+			else:
+				return redirect(url_for('edit', slug=entry.slug))
+		else:
+			flash('Title and content are required.', 'danger')
+	return render_template('create.html')
+
+@app.route('/drafts/')
+@login_required
+def drafts():
+	query = Entry.drafts().order_by(Entry.timestamp.desc())
+	return object_list('index.html', query)
+
+@app.route('/<slug>/')
+def detail(slug):
+	if session.get('logged_in'):
+		query = Entry.select()
+	else:
+		query = Entry.public()
+	entry = get_object_or_404(query, Entry.slug == slug)
+	return render_template('detail.html', entry=entry)
+
+@app.route('/<slug>/edit/', methods=['GET', 'POST'])
+@login_required
+def edit(slug):
+	entry = get_object_or_404(Entry, Entry.slug == slug)
+	if request.method == 'POST':
+		if request.form.get('title') andd request.form.get('content'):
+			entry.title = request.form['title']
+			entry.content = request.form['content']
+			entry.published =  request.form.get('published') or False
+			entry.save()
+
+			flash('Entry saved successfully.', 'sucess')
+			if entry.published:
+				return redirect(url_for('detail', slug=entry.slug))
+			else:
+				return redirect(url_for('edit', slug=entry.slug))
+		else:
+			flash('Title and content are required.', 'danger')
+
+	return render_template('edit.html',entry=entry)
+
+@app.template_filter('clean_querystring')
+def clean_querystring(request_args, *keys_to_remove, **new_values):
+	querystring = dict((key, value) for key, value in request_args.items())
+	for key in keys_to_remove:
+		querystring.pop(key, None)
+	querystring.update(new_values)
+	return urllib.urlencode(querystring)
+
+@app.errorhandler(404)
+def not_found(exc):
+	return Response('<h3>Page not found</h3>'), 404
+
 
 def main():
 	database.create_tables([Entry, FTSEntry], safe=True)
