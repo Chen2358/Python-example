29ab095ed85f36c83059521ffcfdd23c5a1ad110
Chen2358 <1430535115@qq.com>
2018-08-24 09:39:43 +0800

    Update util.py

--- markup/util.py
+++ markup/util.py
@@ -5,10 +5,12 @@
 文本块生成器：把纯文本分成一个个文本块
 '''
 def lines(file):
+	#生成器，在文本最后加一空行
 	for line in file: yield line
 	yield '\n'
 
 def blocks(file):
+	#生成器，生成单独的文本块
 	block = []
 	for line in lines(file):
 		if line.strip():
