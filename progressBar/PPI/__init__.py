be7d1a66645d374ce898ba3e0e2f694ef7092535
Chen2358 <1430535115@qq.com>
2018-09-03 20:02:44 +0800

    Update __init__.py

--- progressBar/PPI/__init__.py
+++ progressBar/PPI/__init__.py
@@ -1,3 +1,5 @@
+#为了访问类PPI.ProgBar, PPI.PRogPercent和函数PPI.prog_bar, PPI.prog_percent 将名称导入包PPI 的命名空间
+
 from .progbar import ProgBar
 from .progpercent import ProgPercent
 from .generator_factory import prog_percent
