add6ff6a62b6d352eede25372227567046f6c6ea
Chen2358 <1430535115@qq.com>
2018-09-05 18:12:07 +0800

    Update constants.py

--- pythonShell/func/constants.py
+++ pythonShell/func/constants.py
@@ -1,7 +1,10 @@
+'''
+定义各种常量和路径
+'''
 import os
 
-SHELL_STATUS_STOP = 0
-SHELL_STATUS_RUN = 1
+SHELL_STATUS_STOP = 0 # Shell 进程停止标识
+SHELL_STATUS_RUN = 1 #Shell 进程运行标识
 
-#
-HISTORY_PATH = os.path.expanduser('~') + os.sep + '.chen_history'
+# 使用ps.path.expanduser('~') 获取当前操作系统平台的当前用户目录
+HISTORY_PATH = os.path.expanduser('~') + os.sep + '.shell_history' #命令日志的存储路径
