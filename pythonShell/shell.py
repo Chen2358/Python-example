744d71f197670dbd16818d7868f349363c1bd627
Chen2358 <1430535115@qq.com>
2018-09-05 17:47:59 +0800

    Update shell.py

--- pythonShell/shell.py
+++ pythonShell/shell.py
@@ -12,15 +12,21 @@ import platform
 from func import *
 
 
-# 
+# 字典表，用于存储命令与函数的映射
 built_in_cmds = {}
 
-
+#解析命令
 def tokenize(string):
+	'''
+	. 将 sting 按shell 的语法规则进行分割；即按空格符将命令与参数分开
+	. 返回 string 的分割列表
+	. 例如 ls -l /home 分割后:['ls', '-l', '/home']
+	'''
 	return shlex.split(string)
 
-
+#预处理
 def preprocess(tokens):
+	#用于存储处理后的token
 	processed_token = []
 	for token in tokens:
 		if token.startswith('$'):
@@ -52,25 +58,34 @@ def execute(cmd_tokens):
 			os.system(command)
 	return SHELL_STATUS_RUN
 
-
+#打印命令提示符
 def display_cmd_prompt():
+	#获取当前用户名
 	user = getpass.getuser()
+	#获取当前运行Python 程序的机器的主机名
 	hostname = socket.gethostname()
+	#获取当前工作路径
 	cwd = os.getcwd()
+	#获取路径cwd 的最低一级目录
 	base_dir = os.path.basename(cwd)
+	#如果用户当前位于用户的根目录下，使用‘~’ 代替根目录
 	home_dir = os.path.expanduser('~')
 	if cwd == home_dir:
 		base_dir = '~'
+	#输出命令提示符
 	if platform.system() != 'Windows':
 		sys.stdout.write("[\033[1;33m%s\033[0;0m@%s \033[1;36m%s\033[0;0m]$" % (user, hostname, base_dir))
 	else:
 		sys.stdout.write("[%s@%s %s]$" % (user, hostname, base_dir))
+	#强制刷新
 	sys.stdout.flush()
 
-
+#忽略信号
 def ignore_signals():
 	if platform.system() != "Windows":
+		#忽略Crtl-Z 信号
 		signal.signal(signal.SIGTSTP, signal.SIG_IGN)
+	#忽略Ctrl-C 信号
 	signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 
@@ -78,24 +93,27 @@ def shell_loop():
 	status = SHELL_STATUS_RUN
 
 	while status == SHELL_STATUS_RUN:
-		#
+		# 打印命令提示符，形如 `[<user>@<hostname> <base_dir>]$`
 		display_cmd_prompt()
 
-		#
+		# 忽略 Ctrl-Z 或 Ctrl-C 信号
 		ignore_signals()
+		
 		try:
-			#
+			# 读取命令
 			cmd = sys.stdin.readline()
 
-			#
+			#解析命令，将命令拆分，返回一个列表
 			cmd_tokens = tokenize(cmd)
 
-			#
+			#预处理函数
+			#将命令中的环境变量用真实值替换
 			cmd_tokens = preprocess(cmd_tokens)
-			#
+			# 执行命令并返回 shell 的状态
 			status = execute(cmd_tokens)
 		except:
-			#
+			#错误处理
+			#sys.exc_info() 函数返回一个包含三个值的元祖（type，value，traceback），这三个值产生于最近一次被处理的异常
 			_, err, _ = sys.exc_info()
 			print(err)
 
@@ -103,15 +121,15 @@ def shell_loop():
 
 def register_command(name, func):
 	'''
-
-	@param name:
-	@param func:
+	注册命令，使命令和相应的函数建立映射关系
+	@param name: 命令名
+	@param func: 函数名
 	'''
 	built_in_cmds[name] = func
 
 def init():
 	'''
-
+	注册命令
 	'''
 	register_command("cd", cd)
 	register_command("exit", exit)
@@ -121,9 +139,9 @@ def init():
 
 
 def main():
-	#
+	#初始化，即建立命令与函数映射关系
 	init()
-	#
+	#处理命令主程序
 	shell_loop()
 
 if __name__ == '__main__':
