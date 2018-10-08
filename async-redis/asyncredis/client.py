6e5a55b772f48f18a6e7bbd607304317bd6da6a7
Chen2358 <1430535115@qq.com>
2018-10-08 11:49:57 +0800

    Update client.py

--- async-redis/asyncredis/client.py
+++ async-redis/asyncredis/client.py
@@ -8,7 +8,8 @@ class Client(object):
 		self.password = password
 		self.db = db or 0
 		#TODO
-
+	
+	#格式化命令
 	def format_command(self, *tokens, **kwargs):
 		cmds = []
 		for t in tokens:
@@ -17,7 +18,7 @@ class Client(object):
 			cmds.append("$%s\r\n%s\r\n" % (len(e_t), e_t_s))
 		return "*%s\r\n%s" % (len(tokens), "".join(cmds))
 
-	#
+	#格式化返回数据
 	def format_reply(self, cmd_line, data):
 		cmd = cmd_line.cmd
 		#
@@ -32,12 +33,12 @@ class Client(object):
 		else:
 			return data
 
-	#
+	#执行命令
 	@gen.coroutine
 	def execute_command(self, cmd, *args, **kwargs):
 		result = None
 		com_line =CmdLine(cmd, *args, **kwargs)
-		#
+		#尝试2次
 		n_tries = 2
 		while n_tries > 0:
 			n_tries -= 1
@@ -45,16 +46,16 @@ class Client(object):
 				self.connection.connect()
 
 			if cmd not in ("AUT", "SELECT"):
-				#
+				#进行认证
 				if self.password and self.connection.info.get("pass", None) != self.password:
 					yield self.auth(self.password)
-				#
+				#选择数据库
 				if self.db and self.connection.info.get("db", 0) != self.db:
 					yield self.select(self.db)
-			#
+			#格式命令
 			command = self.format_command(cmd, *args, **kwargs)
 			try:
-				#
+				#发送数据
 				yield self.connection.write(command)
 			except Exception as e:
 				self.connection.disconnect()
@@ -63,7 +64,7 @@ class Client(object):
 				else:
 					continue
 
-			#
+			#读取数据
 			data = yield self.connection.read_line()
 			if not data:
 				if not n_tries:
@@ -78,7 +79,7 @@ class Client(object):
 		#
 		return result
 
-	#
+	#处理 bulk 数据
 	@gen.coroutine
 	def _consume_bulk(self, tail):
 		response = yield self.connection.read(int(tail) + 2)
@@ -91,7 +92,7 @@ class Client(object):
 			response = response[:-2]
 		return response
 
-	#
+	#根据Redis协议判断是否含有后续数据进行处理
 	def process_data(self, data, cmd_line):
 		data = to_basestring(data)
 		data = data[:-2]		# strip \r\n
@@ -121,12 +122,12 @@ class Client(object):
 
 		#TODO
 
-		#
+		#设置Key
 		@gen.coroutine
 		def set(self, key, value):
 			result = yield self.execute_command("SET", key, value)
 			return result
-		#
+		#获取key
 		@gen.coroutine
 		def get(self, key):
 			value = yield self.execute_command('GET', key)
