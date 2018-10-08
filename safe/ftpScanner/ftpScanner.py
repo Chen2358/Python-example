176c7bd2b3c17195c759004860ea1ddf744ca50a
Chen2358 <1430535115@qq.com>
2018-10-08 10:13:13 +0800

    Update ftpScanner.py

--- safe/ftpScanner/ftpScanner.py
+++ safe/ftpScanner/ftpScanner.py
@@ -5,36 +5,36 @@ from ftplib import *
 import argparse
 import time
 
-#
-def anonScan(hostname):				#
+#匿名登录扫描
+def anonScan(hostname):				#参数是主机名
 	try:
-		with FTP(hostname) as ftp:	#
-			ftp.login()				#
-			print('\n[*] ' + str(hostname) + " FTP Anonymous login successful!") 	#
+		with FTP(hostname) as ftp:	#创建FTP对象
+			ftp.login()				#FTP匿名登录
+			print('\n[*] ' + str(hostname) + " FTP Anonymous login successful!") 	#没有抛出异常则登录成功
 			return True
-	except Exception as e:			#
+	except Exception as e:					#抛出异常则登录失败
 		print('\n[]' + str(hostname) + " FTP  Anonymous logon failure!")
 		return False
 
 
-#
-def vlcLogin(hostname, pwdFile):
+#尝试登录
+def vlcLogin(hostname, pwdFile):					
 	try:
-		with open(pwdFile, 'r') as pf:			#
-			for line in pf.readlines():			#
+		with open(pwdFile, 'r') as pf:				#打开字典文件
+			for line in pf.readlines():			#循环读取每一行
 				time.sleep(1)
-				userName = line.split(':')[0]	#
-				passWord = line.split(':')[1].strip('\r').strip('\n')		#
+				userName = line.split(':')[0]		#从读取的内容中取出用户名
+				passWord = line.split(':')[1].strip('\r').strip('\n')		#从读取的内容中取出密码
 				print('[+] Trying: ' + userName + ':' + passWord)
 				try:
-					with FTP(hostname) as ftp: 		#
-						ftp.login(userName, passWord)
+					with FTP(hostname) as ftp: 		#以主机名为参数构造FTP对象
+						ftp.login(userName, passWord)	#使用读取的用户名密码登录FTP服务器
 
-						#
+						#如果没有异常则表示登录成功，打印主机名、用户名和密码
 						print('\n[+] ' +str(hostname) + 'FTP login successful: '+ userName + ':' + passWord)
 						return (userName, passWord)
 				except Exception as e:
-					#
+					#产生异常则失败，尝试其他用户名
 					pass
 	except IOError as e:
 		print('Error: the password file does not exist!')
@@ -43,9 +43,10 @@ def vlcLogin(hostname, pwdFile):
 
 
 def main():
-	#
+	#命令解析
+	#用描述创建了ArgumentParser对象
 	parser =argparse.ArgumentParser(description='FTP Scanner')
-	#
+	#添加-H 命令，dest表示解析时获取-H参数后面值的变量名，help表示帮助信息
 	parser.add_argument('-H', dest='hostName', help='The host list with ","space')
 	parser.add_argument('-f',dest='pwdFile', help='Password dictionary file')
 	options = None
