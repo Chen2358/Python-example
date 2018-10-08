dcb5ad565b4b05314251b0dd017613dcc2bce717
Chen2358 <1430535115@qq.com>
2018-10-08 15:52:13 +0800

    Update login_base.py

--- django_login/login_base.py
+++ django_login/login_base.py
@@ -17,10 +17,12 @@ values = {
 headers = {'Referer': 'http://localhost:8000/polls/show_login'}
 request = urllib2.Request(url, data=urllib.parse.urlencode(values).encode(encoding='UTF-8'), headers=headers)
 
-cookies = cookielib.MozillaCookieJar('my_cookies.txt')
+#创建opener
+cookies = cookielib.MozillaCookieJar('my_cookies.txt')	#指定cookie的存储文件
 cookie_handler = urllib2.HTTPCookieProcessor(cookies)
 opener = urllib2.build_opener(cookie_handler)
 
+#发送请求&保存cookie，response2表示使用登录后的功能
 response = opener.open(request)
 response2 = opener.open(url2)
 cookies.save()
