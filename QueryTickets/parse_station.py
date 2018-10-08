3ba18b48d17d77d0e328909cf96bc348b22f1c0d
Chen2358 <1430535115@qq.com>
2018-10-08 10:47:46 +0800

    Update parse_station.py

--- QueryTickets/parse_station.py
+++ QueryTickets/parse_station.py
@@ -3,6 +3,7 @@ import requests
 from pprint import pprint
 
 url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.8971'
+#verify=False 表示不验证证书
 response =requests.get(url, verify=False)
 stations = re.findall(u'([\u4e00-\u9fa5]+)\|([A-Z]+)',response.text)
 pprint(dict(stations), indent=4)
