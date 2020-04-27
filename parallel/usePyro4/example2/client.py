f785fe96eb0fd1bb2011394e490127298684ad1e
Chen2358 <1430535115@qq.com>
2020-04-27 09:20:54 +0800

    Create client.py

---
+++ parallel/usePyro4/example2/client.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+import Pyro4
+
+
+uri = input("What is the Pyro uri of the greeting object? ").strip()
+name = input("What is your name? ").strip()
+server = Pyro4.Proxy("PYRONAME:server")
+print(server.welcomeMessage(name))
